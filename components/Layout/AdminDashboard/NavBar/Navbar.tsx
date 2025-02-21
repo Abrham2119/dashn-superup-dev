bc6d1cb84b98c19fc4da524167fa5785f26d2dcd
Abrham Zewdu <abrhamzewdu2119@gmail.com>
2025-02-21 09:49:48 +0300

    Update Navbar.tsx
    if logout remove the token from local storage

--- components/Layout/AdminDashboard/NavBar/Navbar.tsx
+++ components/Layout/AdminDashboard/NavBar/Navbar.tsx
@@ -17,15 +17,9 @@ export default function AdminNavbar({ }: Props) {
 
   const handleLogout = async () => {
     setIsLoading(true);
-    try {
-      await handleSignOut();
-      removeOtpToken();
-      removeVerifyToken();
-    } catch (error) {
-      console.error("Error during sign out:", error);
-    } finally {
-      setIsLoading(false);
-    }
+    handleSignOut();
+    removeOtpToken();
+    removeVerifyToken();
   };
 
   return (
